8b6426ed2af47db00e4c5ed1e6f6235bff239b83
anastasija1 <belevska.anastasija@gmail.com>
2023-03-15 21:35:50 +0100

    vezba 2/3

---
+++ vezba2/vezba2.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+#include<cstdlib>
+#include<ctime>
+#include<string>
+using namespace std;
+const int deckSize = 54;
+const int suitSize = 13;
+
+string suits[4] = {"Hearts", "Diamonds", "Clubs", "Spades"};
+string ranks[suitSize]={"Ace", "2","3", "4","5", "6","7", "8", "9","10", "Jack", "Queen", "King"};
+string jokers[2] = {"Black Joker" "Red Joker"};
+
+void shuffleDeck(string deck[],int size){
+    for (int i = 0;i<size;i++){
+        int j = rand () % size;
+        string temp = deck[i];
+        deck[i] = deck[j];
+        deck[j] = temp;
+    }
+}
+void printDeck (string deck[] int size) {
+    for (int i=0; i<deckSize; i++) {
+        cout<<deck[i]<<"\n";
+    }
+}
+int main () {
+    string deck[deckSize];
+    int index=0;
+    //sostavuvanje na spil karti
+    for (int i=0; i<4;i++){
+    for (int j=0; j<suitSize;j++);  {
+    deck [index]  = ranks[j] + "["+ suits[i]+"]";
+    index++;
+    }  
+} 
+deck[index]=jokers[0];
+deck [index+1]=jokers[1];
+
+cout<<"\n Podredeni: \n";
+printDeck (deck,deckSize);
+return 0;
+}
---
+++ vezba3/clock_vezba3.cpp
@@ -0,0 +1,26 @@
+//Anastasija Belevska INKI928
+#include <stdio.h>    
+#include <time.h>       
+#include <math.h>       
+
+int frequency_of_primes (int n) {
+  int i,j;
+  int freq=n-1;
+  for (i=2; i<=n; ++i) for (j=sqrt(i);j>1;--j) if (i%j==0) {--freq; break;}
+  return freq;
+}
+
+int main ()
+{
+  clock_t t;
+  int f;
+  t = clock();
+  printf ("Calculating...\n");
+  f = frequency_of_primes (99999);
+  printf ("The number of primes lower than 100,000 is: %d\n",f);
+  t = clock() - t;
+  printf ("It took me %d clicks (%f seconds).\n",t,((float)t)/CLOCKS_PER_SEC);
+  return 0;
+}
+/*so izvrsuvanje na programata se presemtuva vremeto na izvrsuvanje na programata vo sekundi. za da se presmeta vistinskoto vreme na obrabotka na programata vrednosta od 
+casovnikot ke se sporedi so vrednosta vratena od predhodnoto povikuvanje na istata funkcija*/
